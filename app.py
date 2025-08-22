77e9fbb9d5af17ba07e2c7abcbec5b2ced0fde9e
shahidbmulani <shahid.mulani123@gmail.com>
2025-08-22 12:16:22 +0530

    reverted code error

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 def main():
-print("🚀 CI/CD Pipeline Test Successfulll with updated change two!")
+    print("🚀 CI/CD Pipeline Test Successfulll with updated change two!")
 
 if __name__ == "__main__":
     main()
